5a140902527559f43e212d8fb59443d6c704aa75
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-24 00:46:41 +0000

    [Add]投稿サクセスメッセージの表示機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
       render :new
     end
